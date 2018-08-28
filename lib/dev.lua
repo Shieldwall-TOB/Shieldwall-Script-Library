f90cb0be330d25ffbdda20953dcb97f5b4c7e183
DFlunchbreak <matte901@gmail.com>
2018-08-28 15:02:21 -0400

    adapted vanish's error check

--- lib/dev.lua
+++ lib/dev.lua
@@ -73,3 +73,141 @@ function log_uicomponent_on_click()
     );
 end;
 log_uicomponent_on_click()
+--v [NO_CHECK] function()
+function MOD_ERROR_LOGS()
+--Vanish's PCaller
+    --All credits to vanish
+    local eh = get_eh();
+
+    --v function(func: function) --> any
+    function safeCall(func)
+        --output("safeCall start");
+        local status, result = pcall(func)
+        if not status then
+            MODLOG(tostring(result))
+            MODLOG(debug.traceback());
+        end
+        --output("safeCall end");
+        return result;
+    end
+    
+    --local oldTriggerEvent = core.trigger_event;
+    
+    --v [NO_CHECK] function(...: any)
+    function pack2(...) return {n=select('#', ...), ...} end
+    --v [NO_CHECK] function(t: vector<WHATEVER>) --> vector<WHATEVER>
+    function unpack2(t) return unpack(t, 1, t.n) end
+    
+    --v [NO_CHECK] function(f: function(), argProcessor: function()) --> function()
+    function wrapFunction(f, argProcessor)
+        return function(...)
+            --output("start wrap ");
+            local someArguments = pack2(...);
+            if argProcessor then
+                safeCall(function() argProcessor(someArguments) end)
+            end
+            local result = pack2(safeCall(function() return f(unpack2( someArguments )) end));
+            --for k, v in pairs(result) do
+            --    output("Result: " .. tostring(k) .. " value: " .. tostring(v));
+            --end
+            --output("end wrap ");
+            return unpack2(result);
+            end
+    end
+    
+    -- function myTriggerEvent(event, ...)
+    --     local someArguments = { ... }
+    --     safeCall(function() oldTriggerEvent(event, unpack( someArguments )) end);
+    -- end
+    
+    --v [NO_CHECK] function(fileName: string)
+    function tryRequire(fileName)
+        local loaded_file = loadfile(fileName);
+        if not loaded_file then
+            MODLOG("Failed to find mod file with name " .. fileName)
+        else
+            MODLOG("Found mod file with name " .. fileName)
+            MODLOG("Load start")
+            local local_env = getfenv(1);
+            setfenv(loaded_file, local_env);
+            loaded_file();
+            MODLOG("Load end")
+        end
+    end
+    
+    --v [NO_CHECK] function(f: function(), name: string)
+    function logFunctionCall(f, name)
+        return function(...)
+            MODLOG("function called: " .. name);
+            return f(...);
+        end
+    end
+    
+    --v [NO_CHECK] function(object: any)
+    function logAllObjectCalls(object)
+        local metatable = getmetatable(object);
+        for name,f in pairs(getmetatable(object)) do
+            if is_function(f) then
+                MODLOG("Found " .. name);
+                if name == "Id" or name == "Parent" or name == "Find" or name == "Position" or name == "CurrentState"  or name == "Visible"  or name == "Priority" or "Bounds" then
+                    --Skip
+                else
+                    metatable[name] = logFunctionCall(f, name);
+                end
+            end
+            if name == "__index" and not is_function(f) then
+                for indexname,indexf in pairs(f) do
+                    MODLOG("Found in index " .. indexname);
+                    if is_function(indexf) then
+                        f[indexname] = logFunctionCall(indexf, indexname);
+                    end
+                end
+                MODLOG("Index end");
+            end
+        end
+    end
+    
+    -- logAllObjectCalls(eh);
+    -- logAllObjectCalls(cm);
+    -- logAllObjectCalls(game_interface);
+    
+    eh.trigger_event = wrapFunction(
+        eh.trigger_event,
+        function(ab)
+            --output("trigger_event")
+            --for i, v in pairs(ab) do
+            --    output("i: " .. tostring(i) .. " v: " .. tostring(v))
+            --end
+            --output("Trigger event: " .. ab[1])
+        end
+    );
+    
+    check_callbacks = wrapFunction(
+        check_callbacks,
+        function(ab)
+            --output("check_callbacks")
+            --for i, v in pairs(ab) do
+            --    output("i: " .. tostring(i) .. " v: " .. tostring(v))
+            --end
+        end
+    )
+    
+    local currentAddListener = eh.add_listener;
+    --v [NO_CHECK] function(eh: any, listenerName: any, eventName: any, conditionFunc: any, listenerFunc: any, persistent: any)
+    function myAddListener(eh, listenerName, eventName, conditionFunc, listenerFunc, persistent)
+        local wrappedCondition = nil;
+        if is_function(conditionFunc) then
+            --wrappedCondition =  wrapFunction(conditionFunc, function(arg) output("Callback condition called: " .. listenerName .. ", for event: " .. eventName); end);
+            wrappedCondition =  wrapFunction(conditionFunc);
+        else
+            wrappedCondition = conditionFunc;
+        end
+        currentAddListener(
+            eh, listenerName, eventName, wrappedCondition, wrapFunction(listenerFunc), persistent
+            --eh, listenerName, eventName, wrappedCondition, wrapFunction(listenerFunc, function(arg) output("Callback called: " .. listenerName .. ", for event: " .. eventName); end), persistent
+        )
+    end
+    eh.add_listener = myAddListener;
+end
+
+
