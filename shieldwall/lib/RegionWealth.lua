ed4dddb154602a287087d75a7537d0b024ce6ff2
DF <matte901@gmail.com>
2018-10-07 23:42:29 -0400

    devastation mechanics

--- shieldwall/lib/RegionWealth.lua
+++ shieldwall/lib/RegionWealth.lua
@@ -1,7 +1,7 @@
 WEALTH_BUNDLE_PREFIX = "STRING"
 WEALTH_BUNDLE_MIN = 0
-WEALTH_BUNDLE_MAX = 7
-WEALTH_POINTS_PER_LEVEL = 50
+WEALTH_BUNDLE_MAX = 3
+
 
 local region_wealth_manager = {} --# assume region_wealth_manager: RWM
 
@@ -14,34 +14,56 @@ local self = {}
     })
     --# assume self: RWM
 
-    self._regionsToWealthPoints = {} --:map<string, number>
-    self._maxPoints = WEALTH_BUNDLE_MAX*WEALTH_POINTS_PER_LEVEL
-    self._minPoints = WEALTH_BUNDLE_MIN*WEALTH_POINTS_PER_LEVEL
+    self._maxPoints = WEALTH_BUNDLE_MAX
+    self._minPoints = WEALTH_BUNDLE_MIN
     self._currentLevels = {} --:map<string, number>
-    self._buildingWealthValues = {} --:map<string, number>
+    self._levelTimers = {} --:map<string, number>
 
     _G.rwm = self
 end
 
-
---v function(self: RWM, building: string) --> number
-function region_wealth_manager.get_wealth_value_for_building(self, building)
-    if self._buildingWealthValues[building] == nil then
-        return 0
-    end
-    return self._buildingWealthValues[building]
+--v method(text: any)
+function region_wealth_manager:log(text)
+    MODLOG(tostring(text), "RWM")
 end
+
 
 --v function(self: RWM, region: string) --> number
 function region_wealth_manager.get_region_wealth(self, region)
-    if self._regionsToWealthPoints[region] == nil then
-        self._regionsToWealthPoints[region] = 0
+    if self._currentLevels[region] == nil then
+        self._currentLevels[region] = 0
     end
-    return self._regionsToWealthPoints[region]
+    return self._currentLevels[region]
 end
 
+--v function(self: RWM, region: string) 
+function region_wealth_manager.update_bundle(self, region)
+    self:log("Updating bundles")
+    for i = self._minPoints, self._maxPoints do
+        cm:remove_effect_bundle_from_region(WEALTH_BUNDLE_PREFIX..i, region)
+    end
+    cm:apply_effect_bundle_to_region(WEALTH_BUNDLE_PREFIX..self:get_region_wealth(region), region, 0)
+end
 
+--v function(self: RWM, region: string, value: number)
+function region_wealth_manager.set_region_wealth(self, region, value)
+    self._currentLevels[region] = value
+    self._levelTimers[region] = 2+((value-1)*4)
+    self:update_bundle(region)
+end
 
+--v function(self: RWM, region: string)
+function region_wealth_manager.process_end_turn(self, region)
+    if self._levelTimers[region] > 0 then
+        self._levelTimers[region] = self._levelTimers[region] - 1
+        local timer = self._levelTimers[region]
+        local level = self:get_region_wealth(region)
+        if timer <= ((level-1)*4) then
+            self._currentLevels[region] = self._currentLevels[region] - 1 
+            self:update_bundle(region)
+        end
+    end
+end
 
 
 
