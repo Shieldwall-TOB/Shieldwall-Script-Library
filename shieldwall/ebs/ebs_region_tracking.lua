97b4446d300dad60f5e5443a4fc2fdf392f35aac
DrunkFlamingo <matte901@gmail.com>
2018-11-03 18:01:02 -0400

    updated region tracking to track from game start

--- shieldwall/ebs/ebs_region_tracking.lua
+++ shieldwall/ebs/ebs_region_tracking.lua
@@ -10,3 +10,9 @@ cm:add_listener(
     true
 )
 
+cm:register_first_tick_callback(function()
+    local region_list = dev.region_list()
+    for i = 0, region_list:num_items() -1 do
+        rot:transfer_or_add_region(region_list:item_at(i):name())
+    end
+end)
