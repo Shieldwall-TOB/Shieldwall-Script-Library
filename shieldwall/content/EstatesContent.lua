5e1c83da2d136db075704c53359ca28b43f8cd48
DrunkFlamingo <matte901@gmail.com>
2018-11-10 19:06:22 -0500

    updated estate content for new startpos

--- shieldwall/content/EstatesContent.lua
+++ shieldwall/content/EstatesContent.lua
@@ -1,35 +1,5 @@
 local et = _G.et
 
-local starting_estates = {
-    vik_reg_na_seciri = true,
-    vik_reg_theodford = true,
-    vik_reg_cair_segeint = true,
-    vik_reg_oxnaforda = true,
-    vik_reg_cenannas = true,
-    vik_reg_doneceaster = true,
-    vik_reg_bathanceaster = true,
-    vik_reg_scireburnan = true,
-    vik_reg_staefford = false,
-    vik_reg_dun_eachainn = false,
-    vik_reg_ethandun = false,
-    vik_reg_suthhamtun = false,
-    vik_reg_wiht = false,
-    vik_reg_waecet = false,
-    vik_reg_sceaftesburg = false,
-    vik_reg_cissanbyrig = false,
-    vik_reg_brug = false,
-    vik_reg_ardach = false,
-    vik_reg_dear = false,
-    vik_reg_rinnin = false,
-    vik_reg_celmeresfort = false,
-    vik_reg_sancte_albanes = false,
-    vik_reg_domuc = false,
-    vik_reg_inis_patraic = false,
-    vik_reg_eofesham = false,
-    vik_reg_glastingburi = false,
-    vik_reg_licetfelda = false
-} --:map<string, boolean>
-
 ESTATES_EFFECTS = {
 	["vik_abbey_1"] = { ["building"] = "vik_abbey_1", ["effect"] = "vik_estates_agricultural", ["effect_scope"] = "building_to_building_own", ["value"] = 1, ["value_damaged"] = 1, ["value_ruined"] = 0 },
 	["vik_abbey_2"] = { ["building"] = "vik_abbey_2", ["effect"] = "vik_estates_agricultural", ["effect_scope"] = "building_to_building_own", ["value"] = 1, ["value_damaged"] = 1, ["value_ruined"] = 0 },
@@ -279,13 +249,4 @@ for building, row in pairs(ESTATES_EFFECTS) do
     et:add_estate_to_building(building, row.effect)
 end
 
-
-
-cm:register_first_tick_callback( function()
-    if cm:is_new_game() then
-        for key, value in pairs(starting_estates) do
-            et:create_start_pos_estate(key, value)
-        end
-    end
-end)
 
