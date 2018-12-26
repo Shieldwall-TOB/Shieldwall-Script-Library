13dada82d4fc91684ea2ec8f1e9ecbf4699fa80c
DrunkFlamingo <matte901@gmail.com>
2018-12-25 22:41:26 -0500

    commented constants

--- shieldwall/ShieldWallConstants.lua
+++ shieldwall/ShieldWallConstants.lua
@@ -1,23 +1,22 @@
 return {
-    __write_output_to_logfile = true,
-    __should_output_ui = false,
-    __log_game_objects = false,
-    __should_output_save_load = false,
-    __no_fog = false,
-    __log_settlements = true,
-    __log_characters = true,
-    default_estate_type = "vik_estate_agricultural",
-    noble_estate_type = "vik_estate_estate_building",
-    minor_estate_type = "vik_estate_agricultural",
-    grand_estate_type = "vik_estate_religious",
-    food_storage_cap_absolute = 2000,
-    food_storage_cap_base = 175,
-    food_storage_bundle = "shield_food_storage_bundle_",
-    food_storage_percentage = 0.5,
-    season_bundle_prefix = "shield_season_",
-    charm_title_prefix = "shield_title_",
-    charm_leader_title_prefix = "shield_leader_titles_",
-    titles_localisation = function()
+    __write_output_to_logfile = true, -- write log files
+    __should_output_ui = false, --outputs UI object details on click. Spams the log a bit so leave it off when not doing UI work.
+    __log_game_objects = false, --Logs all game object types to a series of files. For use once per patch.
+    __should_output_save_load = false, --Outputs the internals of the functions which save and load objects. Only necessary for debugging.
+    __no_fog = false, --turn off fog of war.
+    __log_settlements = true, -- log information about settlements when they are selected
+    __log_characters = true, -- log information about characters when they are selected
+    commercial_estate_type = "vik_estate_estate_building", -- the estates_tables key of this type of estate.
+    landed_estate_type = "vik_estate_agricultural", -- the estates_tables key of this type of estate.
+    church_estate_type = "vik_estate_religious", -- the estates_tables key of this type of estate.
+    food_storage_cap_absolute = 2000, --absolute limit any faction can store. 
+    food_storage_cap_base = 175, -- base limit all factions have access to.
+    food_storage_bundle = "shield_food_storage_bundle_", --bundle prefix from effect_bundles_tables
+    food_storage_percentage = 0.5, -- percentage of food stored at the end of each turn
+    season_bundle_prefix = "shield_season_", --bundle prefix from effect_bundles_tables
+    charm_title_prefix = "shield_title_", --bundle prefix from effect_bundles_tables
+    charm_leader_title_prefix = "shield_leader_titles_", --bundle prefix from effect_bundles_tables
+    titles_localisation = function() -- don't touch this. 
         if _G.traits_localized_content == nil then
             return {}
         else return _G.traits_localized_content end 
