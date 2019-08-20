5de2c1bc73c2eab86b4089478f98bdd1d45bb4d7
DrunkFlamingo <38512127+DrunkFlamingo@users.noreply.github.com>
2019-08-20 10:55:34 -0400

    low public order check

--- shieldwall/checks.lua
+++ shieldwall/checks.lua
@@ -16,9 +16,13 @@ local function check_is_faction_viking_faction(faction)
     return not not viking_sc[faction:subculture()]
 end
 
-
+--v function(region: CA_REGION) --> boolean
+local function check_is_region_low_public_order(region)
+    return (region:squalor() - region:sanitation() > 0) 
+end
 
 return {
     is_char_from_viking_faction = check_is_char_from_viking_faction,
-    is_faction_viking_faction = check_is_faction_viking_faction
+    is_faction_viking_faction = check_is_faction_viking_faction,
+    is_region_low_public_order = check_is_region_low_public_order
 }
